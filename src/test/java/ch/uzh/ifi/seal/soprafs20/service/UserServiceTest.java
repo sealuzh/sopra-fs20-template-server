49618c6dacd9b9bfd2a63de4d9f0eebfc5bb66c3
Moritz Eck <moritz.eck@gmail.com>
2020-01-15 12:44:12 +0100

    Updates
    - add: spring-boot-devtools to build.gradle to allow "hot-/live-reloading"
    - update: build.gradle to modern layout
    - update: use gradle 6
    - update: use Junit 5
    - update: use Java 13
    - update: use Jacoco 0.8.5

--- src/test/java/ch/uzh/ifi/seal/soprafs20/service/UserServiceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/service/UserServiceTest.java
@@ -1,17 +1,13 @@
 package ch.uzh.ifi.seal.soprafs20.service;
 
-import ch.uzh.ifi.seal.soprafs20.Application;
 import ch.uzh.ifi.seal.soprafs20.constant.UserStatus;
 import ch.uzh.ifi.seal.soprafs20.entity.User;
 import ch.uzh.ifi.seal.soprafs20.repository.UserRepository;
-import ch.uzh.ifi.seal.soprafs20.service.UserService;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
@@ -20,8 +16,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
  * @see UserService
  */
 @WebAppConfiguration
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes= Application.class)
+@SpringBootTest
 public class UserServiceTest {
 
 
@@ -34,7 +29,7 @@ public class UserServiceTest {
 
     @Test
     public void createUser() {
-        Assert.assertNull(userRepository.findByUsername("testUsername"));
+        Assertions.assertNull(userRepository.findByUsername("testUsername"));
 
         User testUser = new User();
         testUser.setName("testName");
@@ -42,8 +37,8 @@ public class UserServiceTest {
 
         User createdUser = userService.createUser(testUser);
 
-        Assert.assertNotNull(createdUser.getToken());
-        Assert.assertEquals(createdUser.getStatus(),UserStatus.ONLINE);
-        Assert.assertEquals(createdUser, userRepository.findByToken(createdUser.getToken()));
+        Assertions.assertNotNull(createdUser.getToken());
+        Assertions.assertEquals(createdUser.getStatus(),UserStatus.ONLINE);
+        Assertions.assertEquals(createdUser, userRepository.findByToken(createdUser.getToken()));
     }
 }
