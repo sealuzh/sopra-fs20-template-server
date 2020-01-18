6561b5f7366b06dc0aec4b614be9c2591bbf4c91
Moritz Eck <moritz.eck@gmail.com>
2020-01-18 01:14:54 +0100

    Testing
    - add: unit & integration tests + documentation to explain what each test does

--- src/test/java/ch/uzh/ifi/seal/soprafs20/service/UserServiceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/service/UserServiceTest.java
@@ -3,42 +3,38 @@ package ch.uzh.ifi.seal.soprafs20.service;
 import ch.uzh.ifi.seal.soprafs20.constant.UserStatus;
 import ch.uzh.ifi.seal.soprafs20.entity.User;
 import ch.uzh.ifi.seal.soprafs20.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-/**
- * Test class for the UserResource REST resource.
- *
- * @see UserService
- */
-@WebAppConfiguration
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 public class UserServiceTest {
 
-
-    @Qualifier("userRepository")
-    @Autowired
+    @Mock
     private UserRepository userRepository;
 
-    @Autowired
+    @InjectMocks
     private UserService userService;
 
     @Test
-    public void createUser() {
-        Assertions.assertNull(userRepository.findByUsername("testUsername"));
-
+    public void createUser_validInputs_success() {
+        // given
         User testUser = new User();
         testUser.setName("testName");
         testUser.setUsername("testUsername");
 
+        // when -> any object is being save in the userRepository -> return the dummy testUser
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(testUser);
         User createdUser = userService.createUser(testUser);
 
-        Assertions.assertNotNull(createdUser.getToken());
-        Assertions.assertEquals(createdUser.getStatus(),UserStatus.ONLINE);
-        Assertions.assertEquals(createdUser, userRepository.findByToken(createdUser.getToken()));
+        // then
+        Mockito.verify(userRepository, Mockito.times(1)).save(Mockito.any());
+        assertNotNull(createdUser.getToken());
+        assertEquals(UserStatus.ONLINE, createdUser.getStatus());
     }
 }
